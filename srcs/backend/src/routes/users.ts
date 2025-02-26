d768b544f6daf887862277f6579670df968bf6d9
Julius De Baaij <jde-baai@student.codam.nl>
2025-02-26 13:35:05 +0100

    get Schema for getting users

--- srcs/backend/src/routes/users.ts
+++ srcs/backend/src/routes/users.ts
@@ -2,20 +2,42 @@ import allUsers from '../models/users.ts'; // calls the default
 import { users } from '../models/users.ts'; // import a specific const export
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
+// options for get items
+
+const getUserOptions = {
+	schema: {
+		response: {
+			200:
+			{
+				type: 'array',
+				items: {
+					type: 'object',
+					properties: {
+						id: { type: 'number' },// can change to any convenient data type
+						name: { type: 'string' }
+					}
+
+				}
+			}
+		}
+	}
+}
+
+
 function userRoutes(fastify: FastifyInstance, options: any, done: () => void) {
 	// users route displaying all users, using the default export allUser
 	fastify.get('/users', (request: FastifyRequest, reply: FastifyReply) => {
-		reply.send(allUsers); // reply something
+		reply.send(allUsers);
 	});
 
 	// base route displaying base users, using the default export allUser
-	fastify.get('/users/base', (request: FastifyRequest, reply: FastifyReply) => {
-		reply.send(allUsers.users); // reply something
+	fastify.get('/users/base', getUserOptions, (request: FastifyRequest, reply: FastifyReply) => {
+		reply.send(allUsers.users); // ADDED the getUserOptions here, changing the id to string shows impact
 	});
 
 	// admin route displaying admin users, using the default export allUser
 	fastify.get('/users/admins', (request: FastifyRequest, reply: FastifyReply) => {
-		reply.send(allUsers.admins); // reply something
+		reply.send(allUsers.admins);
 	});
 
 	// user route getting a single item, using the specific const export user
