1b1f3f40f14be10c2243cdbf8110e91dd7eed9ce
Julius De Baaij <jde-baai@student.codam.nl>
2025-02-27 16:41:51 +0100

    patched frontend to compile ts files and build with vite, also small backend patch adding the env.ts back

--- srcs/frontend/vite.config.ts
+++ srcs/frontend/vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	root: 'public', // Set the root directory to 'public' for serving the index.html
+	root: 'public',
 	plugins: [
 		tailwindcss(),
 	],
-	// ... existing code ...
 })
