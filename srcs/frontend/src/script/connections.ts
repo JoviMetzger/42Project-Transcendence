04176c93b6d2976ddce080ad54a02a6d53ce6633
Acco Bartelds <abarteld@f1r3s2.codam.nl>
2025-03-20 20:32:34 +0100

    refactored the connnections.ts functions to allow POST requests and more dynamic calling of the connectFunc(). Still need to implement DELETE requests

--- srcs/frontend/src/script/connections.ts
+++ srcs/frontend/src/script/connections.ts
@@ -1,25 +1,36 @@
-async function httpGet(url: string)
+export function requestBody(method:string, content:string | null)
 {
-	return fetch(url)
-	.then((response) => {
-		if (!response.ok)
-			return "GRRR Gimme Valid URL"
-		const contentType = response.headers.get("Content-Type");
-		if (contentType && contentType.includes("application/json"))
-		  	return response.json();
-		else
-			return response.text();
-		
-	})
-	.catch((error) => {
-		console.log(error)
-		return "GRRR Something Weird Happened"
-	})
+	if (method.toUpperCase() === 'GET')
+		return null;
+	if (method.toUpperCase() === 'POST')
+	{
+		return {"method": "POST", "headers": {"Content-Type": "application/json"}, "body": '{' + content + '}'};
+	}
+	if (method.toUpperCase() === 'DELETE')
+		return null;
+	return "ERROR (requestBody): Method '" + method + "'Not Recognized"
 }
 
-export async function connectFunc(url: string){
-	console.log("Connect From " + url);
-	const response = await httpGet(url);
+async function httpGet(url:string, request:any | null)
+{
+	return fetch(url, request)
+	.then((response) => {
+		const contentType = response.headers.get("Content-Type");
+		if (contentType && contentType.includes("application/json"))
+			return response.json();
+		else
+		return response.text();	
+})
+.catch((error) => {
+	console.log(error)
+	return "GRRR Something Weird Happened"
+})
+}
+
+export async function connectFunc(url: string, request:any | null){
+	console.log("Connect To " + url + " Using:")
+	console.log(request)
+	const response = await httpGet(url, request);
 	// console.log(response);
 	return response
 }
