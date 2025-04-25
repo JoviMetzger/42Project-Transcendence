f0b1ab0cb8cd78aa107fced943c1fa4a6064c22d
Julius <julius@debaaij.com>
2025-04-25 14:48:04 +0200

    ignore this

--- srcs/frontend/src/snek/render.ts
+++ srcs/frontend/src/snek/render.ts
@@ -1,9 +1,6 @@
-export class Render {
-	constructor(ctx: CanvasRenderingContext2D, theme: Theme) {
-    this.ctx = ctx;
-    this.theme = theme;
-    this.fps = 60; // aim for 60fps
-    this.counter = 0;
-    this.initTicker();
-}
-}
+import { Application } from 'pixi.js';
+
+const app = new Application();
+
+document.body.appendChild(app.canvas as HTMLCanvasElement);
+
